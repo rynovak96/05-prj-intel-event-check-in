7b23bed53edd2c1bd1968d2db9bf8a03aee67b02
Shyanne Novak <snovak3@asu.edu>
2025-09-27 22:31:37 +0000

    Add js

---
+++ script.js
@@ -0,0 +1,38 @@
+// Get all needed Dom elements
+const form = document.getElementById("checkInForm");
+const nameInput = document.getElementById("attendeeName");
+const teamSelect = document.getElementById("teamSelect");
+
+// Track attendance
+let count = 0; 
+const maxCount = 50; 
+
+// Handle form submission
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  // Get form values
+  const name = nameInput.value;
+  const team = teamSelect.value;
+  const teamName = teamSelect.selectedOptions[0].text;
+
+  console.log(name, team, teamName); 
+
+  // Increment count
+  count++;
+  console.log("Total Check-Ins: " + count);
+
+  // Update progress bar
+  const percentage = Math.round((count/maxCount) * 100) + "%";
+  console.log(`Progress: ${percentage}`); 
+
+  // Update team counter
+  const teamCounter = document.getElementById(team + "Count");
+  teamCounter.textContent = parseInt(teamCounter.textContent) + 1;
+
+  // Show welcome message
+  const message = `🎉 Welcome, ${name} from ${teamName}`
+  console.log(message);
+
+  form.reset(); 
+});
